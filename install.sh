fbba5120d003971ada509e5ff0b25bfbbd7cc7e6
samridht23 <samridht23@gmail.com>
2022-11-30 20:20:39 +0530

    updated

--- install.sh
+++ install.sh
@@ -50,7 +50,12 @@ pkg=(
     "flameshot"
     "i3"
 )
-
+aur_pkg=(
+    "https://aur.archlinux.org/google-chrome.git"
+    "https://aur.archlinux.org/postman-bin.git"
+)
+sudo pacman -Syu
+sudo pacman -Syyu
 
 function pause(){
     read -p "Press any key to continue" && echo -e "\n"
@@ -64,9 +69,10 @@ Cyan='\033[0;36m'
 installed=()
 notinstalled=()
 
+
 for pkg in "${pkg[@]}"; do
     echo -e "${Cyan}Installing:${Nc} ${Green}${pkg}${Nc}"
-    sudo pacman -S "$pkg" --noconfirm 
+    sudo pacman -S "$pkg" --noconfirm --needed
     status=$?
     if test $status != 0 
     then
@@ -75,6 +81,21 @@ for pkg in "${pkg[@]}"; do
         installed+=(${pkg})
     fi
 done
+# ohmyzsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# neovim config
+git clone https://github.com/samridht23/nvim.git ${HOME}/.config
+
+mkdir ${HOME}/apps
+mkdir ${HOME}/Downloads/fonts
+cd ${HOME}/apps
+for val in "${aur_pkg[@]}";do
+    git clone "$val"
+done
+cd
+
+wget -P ${HOME}/Downloads/fonts "https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip"
+wget -P ${HOME}/Downloads/fonts "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf"
 
 for pkg in "${pkg[@]}";do
     status=true
@@ -90,6 +111,5 @@ for pkg in "${pkg[@]}";do
     else 
         printf "${Cyan}%40s${Nc} ${Red}%10s${Nc}\n" $pkg "Error"
     fi
-
 done
 
